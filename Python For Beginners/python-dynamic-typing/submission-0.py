5e6e10357530c89a73502c668aae7878f8d1934a
kadalihiranmayee <84459540+kadalihiranmayee@users.noreply.github.com>
2026-07-27 13:08:25 +0200

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 2
+print(type(variable))
+variable = 3.0
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Hello"
+print(type(variable))
+variable = [2,3,4]
+print(type(variable))
